5fbd46e348cfa54a45c809227d627abef391996a
André Gomes <andgomes95@gmail.com>
2018-10-03 15:18:46 -0300

    fix lower, upper, page_inc and page_size of 3D Shapes blocks

--- mosaicode_lib_c_opengl/extensions/blocks/Types/color.py
+++ mosaicode_lib_c_opengl/extensions/blocks/Types/color.py
@@ -6,7 +6,7 @@ This module contains the FloatValue class.
 from mosaicode.GUI.fieldtypes import *
 from mosaicode.model.blockmodel import BlockModel
 
-class Float(BlockModel):
+class Color(BlockModel):
 
     # -------------------------------------------------------------------------
     def __init__(self):
@@ -15,25 +15,25 @@ class Float(BlockModel):
         self.language = "c"
         self.framework = "opengl"
         self.help = "Not to declare"
-        self.label = "Float"
-        self.color = "200:200:50:150"
+        self.label = "Color"
+        self.color = "200:200:150:150"
         self.group = "Types"
-        self.ports = [{"type":"mosaicode_lib_c_opengl.extensions.ports.float",
-                    "label":"Value",
+        self.ports = [{"type":"mosaicode_lib_c_opengl.extensions.ports.color",
+                    "label":"Color",
                     "conn_type":"Output",
-                    "name":"value"}
+                    "name":"color"}
             ]
-        self.properties = [{"name": "size",
-                            "label": "size",
-                            "type": MOSAICODE_FLOAT,
-                            "lower": -1.0,
-                            "upper": 1.0,
-                            "step": 0.01,
-                            "value": 0.5,
-			    "page_inc": 0.1,
-                            "page_size": 0.1,
-                            }
-                           ]
-        self.codes["declaration"] = """
-        float value$id$ = $prop[size]$;
-"""
+#        self.properties = [{"name": "size",
+#                            "label": "size",
+#                            "type": MOSAICODE_FLOAT,
+#                            "lower": -1.0,
+#                            "upper": 1.0,
+#                            "step": 0.01,
+#                            "value": 0.5,#
+#			    "page_inc": 0.1,
+#                            "page_size": 0.1,
+#                            }
+#                           ]
+#        self.codes["declaration"] = """
+#        float value$id$ = $prop[size]$;
+#"""
