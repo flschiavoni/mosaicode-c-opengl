e2b918a2126a33e4e91cdfc90ec2b04046815651
Flavio Schiavoni <fls@rendera.com.br>
2019-10-29 19:15:22 -0300

    minor changes

--- update-git.sh
+++ update-git.sh
@@ -1,4 +1,4 @@
-git remote add upstream https://github.com/andgomes95/mosaicode-c-opengl.git
+git remote add upstream https://github.com/mosaicode/mosaicode-c-opengl.git
 git checkout master
 git fetch upstream master
 git merge upstream/master
